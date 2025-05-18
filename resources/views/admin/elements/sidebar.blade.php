6174ac7a8dfa81c5f799e38f8bb1e4578778deed
Dungnt1418 <tiendung18292@gmail.com>
2025-05-18 13:54:09 +0700

    update admin menu

--- resources/views/admin/elements/sidebar.blade.php
+++ resources/views/admin/elements/sidebar.blade.php
@@ -1,110 +1,76 @@
-<h2 class="admin-name">
-    {{ Auth::user()->user_name }}
-</h2>
-<div class="box-menu">
-    <h6 class="title-1">
-        <a href="{{route('dashboard')}}">Home</a>
-    </h6>
+<div class="admin-name">
+    <img src="{{ asset('images/logo/logo-admin.png') }}" alt="Đất Phổ Yên" class="">
+</div>
+<div class="box-menu mg-top-20">
+    <div class="row">
+        <div class="col-2">
+            <img src="{{ asset('images/icons/house.png') }}" alt="icon house" class="icon">
+        </div>
+        <div class="col-10">
+            <h6 class="title-1">
+                <a href="{{route('dashboard')}}">Nhà Đất Bán</a>
+            </h6>
+        </div>
+    </div>
 </div>
 <div class="box-menu">
-    <h6 class="title-1">
-        Sản Phẩm
-    </h6>
-    <ul class="">
-        <li>
-            <a href="{{ route('product.index') }}">Danh sách</a>
-        </li>
-        <li>
-            <a href="{{ route('product.create') }}">Thêm mới</a>
-        </li>
-        <li>
-            <a href="{{ route('product.hot') }}">Sản phẩm bán chạy</a>
-        </li>
-        <li>
-            <a href="#">Phụ kiện</a>
-        </li>
-        <li>
-            <a href="{{ route('product.categories') }}">Danh mục</a>
-        </li>
-    </ul>
+    <div class="row">
+        <div class="col-2">
+            <img src="{{ asset('images/icons/house.png') }}" alt="icon house" class="icon">
+        </div>
+        <div class="col-10">
+            <h6 class="title-1">
+                <a href="{{route('dashboard')}}">Nhà Đất Cho Thuê</a>
+            </h6>
+        </div>
+    </div>
 </div>
-
 <div class="box-menu">
-    <h6 class="title-1">
-        Bài viết
-    </h6>
-    <ul class="">
-        <li>
-            <a href="{{ route('page.view', 'gioi-thieu') }}">Giới thiệu</a>
-        </li>
-        <li>
-            <a href="{{ route('page.view', 'khuyen-mai') }}">Khuyến mãi</a>
-        </li>
-        <li>
-            <a href="{{ route('page.view', 'lien-he') }}">Liên hệ</a>
-        </li>
-        <li>
-            <a href="{{ route('page.view', 'huong-dan-mua-hang') }}">Hướng dẫn mua hàng</a>
-        </li>
-    </ul>
+    <div class="row">
+        <div class="col-2">
+            <img src="{{ asset('images/icons/project.png') }}" alt="icon project" class="icon">
+        </div>
+        <div class="col-10">
+            <h6 class="title-1">
+                <a href="{{route('dashboard')}}">Dự Án</a>
+            </h6>
+        </div>
+    </div>
 </div>
-
 <div class="box-menu">
-    <h6 class="title-1">
-        Tin tức
-    </h6>
-    <ul class="">
-        <li>
-            <a href="{{ route('news.index') }}">Danh sách</a>
-        </li>
-        <li>
-            <a href="{{ route('news.create') }}">Thêm mới</a>
-        </li>
-    </ul>
+    <div class="row">
+        <div class="col-2">
+            <img src="{{ asset('images/icons/news.png') }}" alt="icon news" class="icon">
+        </div>
+        <div class="col-10">
+            <h6 class="title-1">
+                <a href="{{route('dashboard')}}">Tin Tức</a>
+            </h6>
+        </div>
+    </div>
 </div>
-
 <div class="box-menu">
-    <h6 class="title-1">
-        Kiến thức
-    </h6>
-    <ul class="">
-        <li>
-            <a href="{{ route('kienThuc.index') }}">Danh sách</a>
-        </li>
-        <li>
-            <a href="{{ route('kienThuc.create') }}">Thêm mới</a>
-        </li>
-    </ul>
+    <div class="row">
+        <div class="col-2">
+            <img src="{{ asset('images/icons/profiles.png') }}" alt="icon profiles" class="icon">
+        </div>
+        <div class="col-10">
+            <h6 class="title-1">
+                <a href="{{route('dashboard')}}">Thông Tin Cá Nhân</a>
+            </h6>
+        </div>
+    </div>
 </div>
-
 <div class="box-menu">
-    <h6 class="title-1">
-        Chính sách
-    </h6>
-    <ul class="">
-        <li>
-            <a href="{{ route('page.view', 'van-chuyen-va-giao-nhan') }}">Vận chuyển và giao nhận</a>
-        </li>
-        <li>
-            <a href="{{ route('page.view', 'bao-hanh') }}">Bảo hành</a>
-        </li>
-        <li>
-            <a href="{{ route('page.view', 'hoan-tien') }}">Hoàn tiền</a>
-        </li>
-    </ul>
-</div>
-
-<div class="box-menu">
-    <h6 class="title-1">
-        Pages
-    </h6>
-    <ul class="">
-        <li>
-            <a href="{{ route('page.index') }}">Danh sách</a>
-        </li>
-        <li>
-            <a href="{{ route('page.create') }}">Thêm mới</a>
-        </li>
-    </ul>
+    <div class="row">
+        <div class="col-2">
+            <img src="{{ asset('images/icons/logout.png') }}" alt="icon logout" class="icon">
+        </div>
+        <div class="col-10">
+            <h6 class="title-1">
+                <a href="{{route('dashboard')}}">Đăng Xuất</a>
+            </h6>
+        </div>
+    </div>
 </div>
 
