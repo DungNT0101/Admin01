231f6ce28dc660d5d610810254111a42d0e28cfe
Dungnt1418 <tiendung18292@gmail.com>
2025-06-11 07:28:51 +0700

    update watching

--- resources/views/client/type-1/page/watching-properties.blade.php
+++ resources/views/client/type-1/page/watching-properties.blade.php
@@ -19,14 +19,21 @@
 <div class="line-shadow"></div>
 
 <div class="page-listing">
-    {{--  Breadcrumb  --}}
-    @include('client.type-1.element.breadcrumb', [
-        'breadcrumbs' => [
-            ['name' => 'Đất bán', 'url' => route('sale.lands')]
-        ]
-    ])
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12 mg-top-20">
+                <h6 class="page-description">Danh sách bất động sản bạn đang theo dõi: <span style="color: #ff0000">{{ $properties->count() }}</span></h6>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-12">
+                @if ($properties->isEmpty())
+                    <p class="no-properties">Bạn chưa theo dõi bất động sản nào.</p>
+                @endif
+            </div>
+        </div>
+    </div>
 
-    {{--  Listing properties  --}}
     @include('client.type-1.element.sale-property.listing-properties')
 
 </div>
